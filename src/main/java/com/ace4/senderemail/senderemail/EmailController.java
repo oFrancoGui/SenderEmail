539f92c76299f0cc88b1230b6fa03ff790e72ecb
Guilherme Ace <guilherme@ace4.com.br>
2024-09-11 08:43:16 -0300

    usar visual code

--- src/main/java/com/ace4/senderemail/senderemail/EmailController.java
+++ src/main/java/com/ace4/senderemail/senderemail/EmailController.java
@@ -1,2 +1,27 @@
-package com.ace4.senderemail.senderemail;public class EmailController {
+package com.ace4.senderemail.senderemail;
+
+
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send-email")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDTO emailRequest) {
+        try {
+            emailService.sendHtmlEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getHtmlBody());
+            return new ResponseEntity<>("Email sent successfully!", HttpStatus.OK);
+        } catch (MessagingException e) {
+            return new ResponseEntity<>("Failed to send email.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/ace4/senderemail/senderemail/EmailService.java
+++ src/main/java/com/ace4/senderemail/senderemail/EmailService.java
@@ -1,2 +1,28 @@
-package com.ace4.senderemail.senderemail;public class EmailService {
+package com.ace4.senderemail.senderemail;
+
+import com.ace4.senderemail.senderemail.EmailRequestDTO;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendHtmlEmail(String to, String subject, String htmlBody) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+        helper.setFrom("your-email@example.com");
+
+        mailSender.send(mimeMessage);
+    }
 }
