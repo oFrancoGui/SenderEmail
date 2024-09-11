f9c06ff201d6f1df4b9c2325a14d7c5f44eff917
Guilherme Ace <guilherme@ace4.com.br>
2024-09-11 08:43:16 -0300

    usar visual code

--- src/main/java/com/ace4/senderemail/senderemail/EmailRequestDTO.java
+++ src/main/java/com/ace4/senderemail/senderemail/EmailRequestDTO.java
@@ -1,2 +1,34 @@
-package com.ace4.senderemail.senderemail;public class EmailRequestDTO {
+package com.ace4.senderemail.senderemail;
+
+
+public class EmailRequestDTO {
+
+    private String to;
+    private String subject;
+    private String htmlBody;
+
+    // Getters and Setters
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getHtmlBody() {
+        return htmlBody;
+    }
+
+    public void setHtmlBody(String htmlBody) {
+        this.htmlBody = htmlBody;
+    }
 }
